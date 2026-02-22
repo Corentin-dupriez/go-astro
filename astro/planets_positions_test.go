f3762d598cddbf292585ce11b725429cc1b5c5a3
Corentin Dupriez <dupriez.c@icloud.com>
2026-02-22 22:42:51 +0200

    Added tests

--- astro/planets_positions_test.go
+++ astro/planets_positions_test.go
@@ -138,3 +138,42 @@ func TestCalculateSunDistance(t *testing.T) {
 		})
 	}
 }
+
+func TestCalculateCartesianCoordinates(t *testing.T) {
+	tests := []struct {
+		name          string
+		trueLongitude float64
+		distance      float64
+		want          CartesianCoordinates
+	}{
+		{"Test 45deg", 45.0, 10.0, CartesianCoordinates{X: 7.071068, Y: 7.071068, Z: 0.0}},
+		{"Test 180deg", 180.0, 8.0, CartesianCoordinates{X: -8.0, Y: 0.0, Z: 0.0}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := CalculateCartesianCoordinates(tt.trueLongitude, tt.distance)
+			if math.Abs(got.X-tt.want.X) > 1e-6 || math.Abs(got.Y-tt.want.Y) > 1e-6 || math.Abs(got.Z-tt.want.Z) > 1e-6 {
+				t.Errorf("got %.6f, %.6f and %.6f, want %.6f, %.6f and %.6f", got.X, got.Y, got.Z, tt.want.X, tt.want.Y, tt.want.Z)
+			}
+		})
+	}
+}
+
+// func TestComputeLocation(t *testing.T) {
+// 	tests := []struct {
+// 		name string
+// 		t    float64
+// 		want Location
+// 	}{
+// 		{"Test Epoch J2000", 0.0, Location{MeanLongitude: 0.0, MeanAnomaly: 0.0, Eccentricity: 0.0167, EquationOfCenter: 0.0, TrueLongitude: 0.0, TrueAnomaly: 0.0, Distance: 0.0}},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			got := ComputeLocation(tt.t)
+// 			if math.Abs(got.MeanLongitude-tt.want.MeanLongitude) > 1e-6 {
+// 				t.Errorf("got %.6f, want %.6f", got.MeanLongitude, tt.want.MeanLongitude)
+// 			}
+// 		})
+// 	}
+// }
